cfeb6c2dd4f3e74f7dd7e61306e0026eb5d4ad99
RazvanNica242715 <242715@buas.nl>
2025-10-23 10:18:59 +0200

    Add new constants for the predict module

--- src/predict/config.py
+++ src/predict/config.py
@@ -9,6 +9,18 @@ EMOTION_MAP: Dict[int, str] = {
     4: "happiness", 5: "sadness", 6: "surprise"
 }
 
+FINE_EMOTION_MAP = {
+    'happiness': ['anticipation', 'satisfaction', 'gratitude', 'admiration'],
+    'sadness': ['disappointment', 'resignation', 'frustration', 'regret'],
+    'fear': ['concern', 'uncertainty', 'apprehension', 'urgency'],
+    'anger': ['frustration', 'resentment', 'rejection', 'impatience'],
+    'surprise': ['confusion', 'amazement', 'curiosity', 'astonishment'],
+    'disgust': ['contempt', 'disappointment', 'disapproval', 'dismissiveness'],
+    'neutral': ['curiosity', 'neutrality', 'acceptance', 'uncertainty']
+}
+
+INTENSITY_LABELS = ['weak', 'moderate', 'intense']
+
 CORE_CHECKPOINT = ARTIFACTS_DIR / 'bert-base'
 FINE_CHECKPOINT = 'facebook/bart-large-mnli'
 DATA_PATH = TRANSLATIONS
